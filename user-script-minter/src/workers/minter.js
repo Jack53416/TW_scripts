eb19bab1092187fe9d113ff8895acbb479c63577
Jacek Byczkiewicz <jck53416@gmail.com>
2020-01-26 18:36:59 +0100

    Adjust minter class to real game environment

--- user-script-minter/src/workers/minter.js
+++ user-script-minter/src/workers/minter.js
@@ -1,4 +1,5 @@
 import {Worker} from '../worker';
+import {Globals} from '../globals';
 
 export class Minter extends Worker {
     /**
@@ -6,10 +7,9 @@ export class Minter extends Worker {
      * @param {string} mintSelector querySelector for a mint button
      * @param {string} coinSelector querySelector fot 'select all coins' button
      */
-    constructor(mintPage, mintSelector, coinSelector) {
+    constructor(mintPage, mintSelector='a#coin_mint_fill_max + input[type=submit]') {
         super(mintPage);
         this.mintSelector = mintSelector;
-        this.coinSelector = coinSelector;
     }
     
     mintCoins() {
@@ -18,15 +18,12 @@ export class Minter extends Worker {
     }
     
     selectAllCoins() {
-        const coinButton = document.querySelector(this.coinSelector);
-        coinButton.click();
+        Globals.unsafeWindow.BuildingSnob.Modes.train.fillMaxMintable();
     }
     
     run() {
         this.validatePage();
+        this.selectAllCoins();
         this.mintCoins();
-        
-        //this.selectAllCoins();
-        
     }
 }
